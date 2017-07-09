b9af0444739201e3b1d16fbe524c92273783ae18
Andrew Price <andrewprice@mail.weber.edu>
2017-07-08 19:11:09 -0600

    add-migration Initial, update-database

---
+++ SilentAuction/Migrations/AuctionContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using SilentAuction.Data;
+
+namespace SilentAuction.Migrations
+{
+    [DbContext(typeof(AuctionContext))]
+    partial class AuctionContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("SilentAuction.Models.Item", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("ItemName");
+
+                    b.Property<string>("ItemType");
+
+                    b.Property<double>("RetailPrice");
+
+                    b.Property<double>("StartingBid");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Item");
+                });
+        }
+    }
+}
