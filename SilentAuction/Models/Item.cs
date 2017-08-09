d1ec96a28e75579a88db4336655d1d0d2d53c4ea
Andrew Price <andrewprice@mail.weber.edu>
2017-08-08 21:31:44 -0600

    Date validated for Offer Expires

--- SilentAuction/Models/Item.cs
+++ SilentAuction/Models/Item.cs
@@ -50,6 +50,9 @@ namespace SilentAuction.Models
 
         public string Terms { get; set; }
 
+        
+        [Display(Name = "Offer Expires")]
+        [DataType(DataType.Date)]
         public string OfferExpires { get; set; }
 
     }
